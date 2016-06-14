acf04114079bf16efe75dcf527b88ad119a66723
Pear Coffee <williandutras15@gmail.com>
2016-06-13 21:33:21 -0300

    Add files via upload

--- desenvolvimento-de-site/js/custom.js
+++ desenvolvimento-de-site/js/custom.js
@@ -1,11 +1,6 @@
-
 jQuery(function () {
-	"use strict";
-    
-    /*global jQuery $*/ 
-	//Slider 
 	$(document).ready(function(){
-		
+
 		jQuery('.skillbar').each(function() {
 			jQuery(this).appear(function() {
 				jQuery(this).find('.count-bar').animate({
@@ -14,7 +9,17 @@ jQuery(function () {
 				var percent = jQuery(this).attr('data-percent');
 				jQuery(this).find('.count').html('<span>' + percent + '</span>');
 			});
-		});	
+		});
 	});
-	
+
 }());
+
+var mauticUrl = 'https://williandutras.mautic.net/';
+var src = mauticUrl + '/mtracking.gif?page_url=' + encodeURIComponent(window.location.href) + '&page_title=' + encodeURIComponent(document.title);
+var img = document.createElement('img');
+img.style.width  = '1px';
+img.style.height  = '1px';
+img.style.display = 'none';
+img.src = src;
+var body = document.getElementsByTagName('body')[0];
+body.appendChild(img);
