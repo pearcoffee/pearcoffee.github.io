2d0128ffb39ae16113a5078e8d6d0a29c8d01b35
Pear Coffee <williandutras15@gmail.com>
2016-06-14 00:46:37 -0300

    Add files via upload

--- desenvolvimento-de-site/js/custom.js
+++ desenvolvimento-de-site/js/custom.js
@@ -14,6 +14,24 @@ jQuery(function () {
 
 }());
 
+$(document).ready(function() {
+	$('#quero-meu-site').click(function() {
+		$('#formulario').fadeIn(1000);
+	});
+})
+
+$(document).ready(function() {
+	$('#quero-meu-site-2').click(function() {
+		$('#formulario').fadeIn(1000);
+	});
+})
+
+$(document).ready(function() {
+	$('#quero-meu-site-3').click(function() {
+		$('#formulario').fadeIn(1000);
+	});
+})
+
 var mauticUrl = 'https://williandutras.mautic.net';
 var src = mauticUrl + '/mtracking.gif?page_url=' + encodeURIComponent(window.location.href) + '&page_title=' + encodeURIComponent(document.title);
 var img = document.createElement('img');
@@ -23,3 +41,19 @@ img.style.display = 'none';
 img.src = src;
 var body = document.getElementsByTagName('body')[0];
 body.appendChild(img);
+
+    if (typeof MauticSDKLoaded == 'undefined') {
+        var MauticSDKLoaded = true;
+        var head            = document.getElementsByTagName('head')[0];
+        var script          = document.createElement('script');
+        script.type         = 'text/javascript';
+        script.src          = 'https://williandutras.mautic.net/mautic/media/js/mautic-form.js';
+        script.onload       = function() {
+            MauticSDK.onLoad();
+        };
+        head.appendChild(script);
+        var MauticDomain = 'https://williandutras.mautic.net';
+        var MauticLang   = {
+            'submittingMessage': "Por favor aguarde..."
+        }
+    }
